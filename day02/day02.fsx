c8acb6508b00216b336cc4a7c2f60ce4d3444375
OverflowCat <overflowcat@gmail.com>
2024-01-13 01:12:15 +0800

    Day 2 Part 2

--- day02/day02.fsx
+++ day02/day02.fsx
@@ -17,13 +17,38 @@ let testSet (set: string) =
     && (parsed.TryFind("green") |> Option.defaultValue 0) <= 13
     && (parsed.TryFind("blue") |> Option.defaultValue 0) <= 14
 
+let testGame (sets: string) =
+    let sets = sets.Trim().Split(';')
+    sets |> Array.forall testSet
+
 let parseLine (line: string) =
     let parts = line.Split([| ':' |], 2)
     let gameId = int (parts.[0].Trim().Split(' ')[1])
-    let sets = parts.[1].Trim().Split(';')
-    if sets |> Array.forall testSet then Some(gameId) else None
+    if testGame parts.[1] then Some gameId else None
 
 let input: string array = System.IO.File.ReadAllText("./input.txt").Split('\n')
 let sum = input |> Array.choose parseLine |> Array.sum
 
+// assert (sum = 2776)
 printfn "%A" sum
+
+let getRequirement (set: string) =
+    let parsed = parseSet set
+    let red = parsed.TryFind("red") |> Option.defaultValue 0
+    let green = parsed.TryFind("green") |> Option.defaultValue 0
+    let blue = parsed.TryFind("blue") |> Option.defaultValue 0
+    red, green, blue
+
+let getMinPower (sets: string) =
+    let sets = sets.Trim().Split(';')
+    // get maximum value of red, green, blue
+    let red, green, blue = sets |> Array.map getRequirement |> Array.reduce (fun (r1, g1, b1) (r2, g2, b2) -> max r1 r2, max g1 g2, max b1 b2)
+    red * green * blue
+
+let parseLine' (line: string) =
+    let parts = line.Split([| ':' |], 2)
+    getMinPower parts.[1]
+
+let sum' = input |> Array.map parseLine' |> Array.sum
+
+printfn "%A" sum'
