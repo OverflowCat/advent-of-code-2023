40daabd2694d0e0fddc4a1b5a0d9bed9f94055d1
ᡥᠠᡳᡤᡳᠶᠠ ᡥᠠᠯᠠ·ᠨᡝᡴᠣ 猫 <overflowcat@gmail.com>
2024-01-22 01:03:12 +0800

    Day 8 Part 2

--- day08/ghost.rb
+++ day08/ghost.rb
@@ -1,42 +1,42 @@
 lines = File.readlines('input.txt')
-hashmap = {}
+$hashmap = {}
 i = 0
-ops = ""
+$ops = ""
 lines.each do |line|
   line = line.chomp
   if i == 0
-    ops = line
+    $ops = line
   elsif i == 1
     # do nothing
   else
     node, children = line.split(" = ")
-    hashmap[node] = children.tr('()', '').split(", ")
+    $hashmap[node] = children.tr('()', '').split(", ")
   end
   i += 1
 end
 
-nodes_ending_in_A = hashmap.keys.select { |node| node.end_with?("A") }
-print nodes_ending_in_A
-
-current = nodes_ending_in_A
-steps = 0
-while true
-  ops.each_char do |op|
-    current.map! do |currentitem|
-      node = hashmap[currentitem]
+def f(node)
+  current = node
+  steps = 0
+  while true
+    $ops.each_char do |op|
+      node = $hashmap[current]
       if op == "L"
-        currentitem = node[0]
+        current = node[0]
       else
-        currentitem = node[1]
+        current = node[1]
       end
       steps += 1
-      currentitem
+      if current.end_with?("Z")
+        return steps
+      end
     end
-    print current
-    if current.all? { |item| item.end_with?("Z") }
-      puts steps
-      exit
-    end
-    puts steps
   end
 end
+
+nodes_ending_with_A = $hashmap.keys.select { |k| k.end_with?("A") }
+results = nodes_ending_with_A.map { |node| f(node) }
+print results
+puts
+lcm = results.reduce(:lcm)
+puts lcm
